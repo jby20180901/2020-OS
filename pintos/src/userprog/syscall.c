76e1ac3ee4e336ea2ada8368f8e2a62e6fba95d2
jby20180901 <2123189960@qq.com>
2020-11-22 10:38:16 +0800

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -129,6 +129,19 @@ void SysExit(struct intr_frame *f)
   thread_exit();
 }
 
+/*写文件系统调用*/
+/*int fd, char *buffer, unsigned size*/
+void SysWrite(struct intr_frame *f)
+{
+  int *esp = f->esp;
+  int fd = (int)getArguments(f, 2);
+  const char *buffer = (const char *)*(esp + 6);
+  unsigned size = (size_t)getArguments(f, 3);
+  //printf("file descriptor:%d, second arguments:%x, buffer size:%d, fourth arguments:%d, fifth arguments:%d, sixth argument:%d\n", fd, buffer,size,*(esp+4), *(esp+5), *(esp+6));
+  putbuf(buffer, size);
+  f->eax = 0;
+}
+
 // static void
 // syscall_handler(struct intr_frame *f)
 // {
