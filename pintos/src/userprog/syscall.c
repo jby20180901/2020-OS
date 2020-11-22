155a90c530a6a257e543658f367a3c68eeb592de
jby20180901 <48289399+jby20180901@users.noreply.github.com>
2020-11-21 18:47:25 -0800

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -32,14 +32,14 @@ void SysExit(struct intr_frame *);
 /* 用来应对Write系统调用*/
 void SysWrite(struct intr_frame *);
 
-/*用来应对create系统调用*/
-void SysCreate(struct intr_frame *f);
+// /*用来应对create系统调用*/
+// void SysCreate(struct intr_frame *f);
 
-/*用来应对open系统调用*/
-void SysOpen(struct intr_frame *f);
+// /*用来应对open系统调用*/
+// void SysOpen(struct intr_frame *f);
 
-/*用来应对close系统调用*/
-void SysClose(struct intr_frame *f);
+// /*用来应对close系统调用*/
+// void SysClose(struct intr_frame *f);
 /*d Reads a byte at user virtual address UADDR.
    UADDR must be below PHYS_BASE.
    Returns the byte value if successful, -1 if a segfault
@@ -77,9 +77,20 @@ syscall_init (void)
   handlers[SYS_HALT] = SysHalt;
   handlers[SYS_WRITE] = SysWrite;
   handlers[SYS_EXIT] = SysExit;
-  handlers[SYS_CREATE] = SysCreate;
-  handlers[SYS_OPEN] = SysOpen;
-  handlers[SYS_CLOSE] = SysClose;
+  // handlers[SYS_CREATE] = SysCreate;
+  // handlers[SYS_OPEN] = SysOpen;
+  // handlers[SYS_CLOSE] = SysClose;
+}
+
+static void
+syscall_handler (struct intr_frame *f) 
+{
+  //printf ("system call!\n");
+  //shutdown_power_off();
+  validateAddr((const void*)f->esp);
+  int call_number = getSyscallNumber(f);
+  handlers[call_number](f);
+  //thread_exit();
 }
 
 /*用来判断系统调用给我的地址有没有问题*/
@@ -142,66 +153,72 @@ void SysWrite(struct intr_frame *f)
   f->eax = 0;
 }
 
-//   case SYS_WRITE:
-//   {
-//     char *tempesp = (char *)f->esp;
+// /*创建一个新文件*/
+// /*char* file_name, unsigned initial_size*/
+// void SysCreate(struct intr_frame *f){
+//   const char* file_name = (char*)getArguments(f,4);
+//   validateAddr(file_name);
+//   unsigned initial_size = getArguments(f,2);
+//   //printf("file_name:%s\n", file_name);
+//   bool ok = filesys_create(file_name, initial_size);
+//   f->eax = (int)ok;
+// }
 
-//     tempesp += 4;
-//     int fd = *(int *)tempesp;
-//     tempesp += 4;
-//     if (*(uint32_t *)tempesp > PHYS_BASE)
-//     {
-//       thread_current()->returnstatus = -1;
-//       thread_exit();
-//     }
-
-//     char *buffer = *(char **)tempesp;
-//     tempesp += 4;
-//     unsigned size = *(unsigned *)tempesp, temp;
-//     if (fd == 1)
-//     {
-
-//       while (size > 100)
-//       {
-//         putbuf(buffer, 100);
-//         buffer += 100;
-//         size -= 100;
-//         barrier();
-//       }
-//       putbuf(buffer, size);
-//     }
-//     else
-//     {
-//       if (fd <= 0)
-//       {
-//         thread_current()->returnstatus = -1;
-//         thread_exit();
-//       }
-//       else if (fd > sizeof(thread_current()->fdtable) / sizeof(struct file *))
-//       {
-//         thread_current()->returnstatus = -1;
-//         thread_exit();
-//       }
-//       else if (thread_current()->fdtable[fd] == NULL)
-//       {
-//         thread_current()->returnstatus = -1;
-//         thread_exit();
-//       }
-//       else if ((get_user((uint8_t *)buffer) == -1) || (buffer == NULL))
-//       {
-//         thread_current()->returnstatus = -1;
-//         thread_exit();
-//       }
-
-//       struct file *filetowrite = thread_current()->fdtable[fd];
-//       lock_acquire(&handlesem);
-//       off_t writebytes = file_write(filetowrite, (void *)buffer, (off_t)size);
-//       lock_release(&handlesem);
-//       f->eax = (int)writebytes;
-//     }
-//     barrier();
-//     break;
+// /*打开一个文件,获取它的文件描述符*/
+// /*char* filename*/
+// void SysOpen(struct intr_frame *f){
+//   const char *file_name = (char*)getArguments(f,1);
+//   validateAddr(file_name);
+//   lock_acquire(&filesys_lock);
+//   struct file* new_file_position = filesys_open(file_name);
+//   lock_release(&filesys_lock);
+//   if(new_file_position==NULL){
+//     f->eax = -1;
 //   }
+//   else{
+//     struct thread *cur = thread_current();
+//     struct opened_file *new_file = malloc(sizeof(struct opened_file));
+//     //printf("current max fd:%d\n", cur->max_fd);
+//     new_file->fd = (cur->max_fd)+1;
+//     new_file->position = new_file_position;
+//     cur->max_fd++;
+//     list_push_back(&cur->list_opened_file, &new_file->node);
+//     //printf("file descriptor:%d\n", new_file->fd);
+//     f->eax = new_file->fd;
+//   }
+// }
+
+// /*据文件描述符关闭一个文件*/
+// /*int fd*/
+// void SysClose(struct intr_frame *f){
+//   int fd = (int)getArguments(f,1);
+//   if(fd<=1){
+//     exit(-1);
+//   }
+//   else{
+//     struct list_elem *file_node = get_file_by_fd(fd);
+//     //printf("target fd:%d\n", fd);
+//     if(file_node!=NULL){
+//       //printf("not null!\n");
+//       struct opened_file *to_be_closed = list_entry(file_node, struct opened_file, node);
+//       list_remove(file_node);
+//       lock_acquire(&filesys_lock);
+//       file_close(to_be_closed->position);
+//       lock_release(&filesys_lock);
+//     }
+//     else{
+//       //printf("not found!\n");
+//       exit(-1);
+//     }
+//   }
+//   f->eax = 0;
+// }
+/*从一个进程中退出*/
+void exit(int status){
+  printf("%s: exit(%d)\n", thread_current()->name, status);
+  thread_current()->ret = status;
+  thread_exit();
+}
 
 // static void
 // syscall_handler(struct intr_frame *f)
