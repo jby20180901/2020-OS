66d0de3fdb1134a6916d10055045dd75e1d2284b
jby20180901 <2123189960@qq.com>
2020-11-22 22:09:32 +0800

    Update thread.h

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -104,12 +104,16 @@ struct thread
     int nextfd;                         /* 下一个文件指针 */
     struct list childrenlist;           /* 子进程列表 */
     struct list_elem child_elem;        /* 作为一个子进程，存在父进程的elem */
+
     struct semaphore startLoadSem;      /* 子进程加载信号量 */
-    struct semaphore returnLoadSem;      /* 子进程是否加载成功 */
+    struct semaphore returnLoadSem;     /* 子进程是否加载成功 */
+    
     struct semaphore waitsem;           /* 父进程等待子进程的信号量 */
-    struct semaphore dieSem;            /* 进程死亡信号量 */
     struct semaphore recycleSem;        /* 父进程等待回收子进程的资源 */
-    struct semaphore inforDeathSem;            /*  */
+
+    struct semaphore dieSem;            /* 进程死亡信号量 */
+    struct semaphore inforDeathSem;     /* 通知父进程子进程死亡的信号量 */
+
     bool loadsuccess;                   /* 是否load进程成功的标志 */
     struct file *file;                  /* 当前打开的文件 */
     bool wait;                          /* 是否在等待子进程 */
