9d026ab350256f3787a40ae1039aa7bd0d87e7f1
jby20180901 <48289399+jby20180901@users.noreply.github.com>
2020-11-29 06:16:47 -0800

    init

---
+++ pintos/src/userprog/build/userprog/syscall.d
@@ -0,0 +1,8 @@
+userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
+ ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+ ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
+ ../../lib/kernel/stdio.h ../../lib/syscall-nr.h ../../lib/kernel/list.h \
+ ../../threads/interrupt.h ../../threads/thread.h ../../threads/synch.h \
+ ../../filesys/filesys.h ../../filesys/off_t.h ../../filesys/file.h \
+ ../../threads/vaddr.h ../../threads/loader.h ../../devices/shutdown.h \
+ ../../userprog/pagedir.h ../../devices/input.h
